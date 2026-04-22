e4bfb7412b1d9d523230ce7a3918a7790e425bfb
Andronic Dragos Mihai <dragos.andronic2210@stud.acs.upb.ro>
2026-04-22 17:33:57 +0300

    Fix: am organizat organizat in main task 1 si task 2

--- src/interviu1.c
+++ src/interviu1.c
@@ -3,32 +3,43 @@
 int main(int argc, char *const argv[])
 {
     FILE *fin = fopen(argv[1], "r");
-    int n;
-    Node *head = NULL;
-    fscanf(fin, "%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-        double val;
-        fscanf(fin, "%lf", &val);
-        adaugare_final(&head, val);
-    }
+    double v1, v2, v3;
+    int n_citire = fscanf(fin, "%lf %lf %lf", &v1, &v2, &v3);
     fclose(fin);
-    if (n < 2)
+    if (n_citire == 3)
     {
+        }
+    else
+    {
+        fin = fopen(argv[1], "r");
+        int n;
+        Node *head = NULL;
+        fscanf(fin, "%d", &n);
+        for (int i = 0; i < n; i++)
+        {
+            double val;
+            fscanf(fin, "%lf", &val);
+            adaugare_final(&head, val);
+        }
+        fclose(fin);
+        if (n < 2)
+        {
+            free_list(head);
+            return 0;
+        }
+        calculeaza_Rt(head);
+        double Rm = calculeaza_Rm(head, n);
+        double Vol = calculeaza_Volatilitatea(head, n, Rm);
+        double Sharpe = 0.0;
+        if (Vol != 0)
+            Sharpe = Rm / Vol;
+        FILE *fout = fopen(argv[2], "w");
+        afisare_trunchiata(fout, Rm);
+        afisare_trunchiata(fout, Vol);
+        afisare_trunchiata(fout, Sharpe);
+        fclose(fout);
         free_list(head);
-        return 0;
     }
-    calculeaza_Rt(head);
-    double Rm = calculeaza_Rm(head, n);
-    double Vol = calculeaza_Volatilitatea(head, n, Rm);
-    double Sharpe = 0.0;
-    if (Vol != 0)
-        Sharpe = Rm / Vol;
-    FILE *fout = fopen(argv[2], "w");
-    afisare_trunchiata(fout, Rm);
-    afisare_trunchiata(fout, Vol);
-    afisare_trunchiata(fout, Sharpe);
-    fclose(fout);
-    free_list(head);
+
     return 0;
 }
